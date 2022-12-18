917e5003b310857cb1d0c492f99ea6fc7f156ffc
Alexander Wang <alex@terrastruct.com>
2022-12-17 17:49:49 -0800

    lib/stubs

--- src/js/modules/editor.js
+++ src/js/modules/editor.js
@@ -8,6 +8,7 @@ import Zoom from "./zoom.js";
 import Alert from "./alert.js";
 
 import QueryParams from "../lib/queryparams";
+import Stubs from "../lib/stubs";
 
 const MAX_ERRORS = 5;
 
@@ -274,7 +275,7 @@ async function compile() {
     }
     svg = await response.text();
   } else {
-    svg = `<!--?xml version="1.0" encoding="utf-8"?--><svg style="background: white; transform-origin: 0px 0px 0px; transform: matrix(1, 0, 0, 1, 0, 0);" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" width="701px" height="586.203125px" viewBox="-100 -100 314 552" id="diagram"><g id="x"><g class="shape"><rect x="1" y="0" width="113" height="126" style="fill:#F7F8FE;stroke:#0D32B2;opacity:1.000000;stroke-width:2;"></rect></g><text class="text-bold" x="57.500000" y="66.000000" style="text-anchor:middle;font-size:16px;fill:#0A0F25">dev</text></g><g id="y"><g class="shape"><rect x="0" y="226" width="114" height="126" style="fill:#F7F8FE;stroke:#0D32B2;opacity:1.000000;stroke-width:2;"></rect></g><text class="text-bold" x="57.000000" y="292.000000" style="text-anchor:middle;font-size:16px;fill:#0A0F25">mode</text></g><g id="(x -> y)[0]"><marker id="mk-3990223579" markerWidth="10.000000" markerHeight="12.000000" refX="7.000000" refY="6.000000" viewBox="0.000000 0.000000 10.000000 12.000000" orient="auto" markerUnits="userSpaceOnUse"> <polygon class="connection" fill="#0D32B2" stroke-width="2" points="0.000000,0.000000 10.000000,6.000000 0.000000,12.000000"></polygon> </marker><path d="M 57.000000 128.000000 C 57.000000 166.000000 57.000000 186.000000 57.000000 222.000000" class="connection" style="fill:none;stroke:#0D32B2;opacity:1.000000;stroke-width:2;" marker-end="url(#mk-3990223579)" mask="url(#2109136295)"></path></g><style type="text/css"></style></svg>`
+    svg = Stubs.DUMMY_SVG;
     hideLoader();
     unlockCompileBtn();
   }
@@ -283,6 +284,7 @@ async function compile() {
   const containerHeight = renderEl.getBoundingClientRect().height;
   renderEl.innerHTML = svg;
 
+  // skip over the xml version tag
   const svgEl = renderEl.lastChild;
 
   svgEl.id = "diagram";
