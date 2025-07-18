ad55a5b60a0ab2a1bcc906b44cf7260df36ac5a5
Alexander Wang <alex@terrastruct.com>
2025-07-18 15:06:48 -0600

    copy to clipboard

--- src/js/modules/alert.js
+++ src/js/modules/alert.js
@@ -2,16 +2,23 @@
 // TODO add a dismiss button
 let timeout;
 
-function show(msg, duration) {
+function show(msg, duration, type = "error") {
   if (timeout) {
     clearTimeout(timeout);
   }
 
-  document.getElementById("notification").classList.remove("notification-inactive");
+  const notification = document.getElementById("notification");
+  notification.classList.remove("notification-inactive", "notification-success");
+
+  if (type === "success") {
+    notification.classList.add("notification-success");
+  }
+
   document.getElementById("notification-message").innerHTML = msg;
 
   timeout = setTimeout(() => {
-    document.getElementById("notification").classList.add("notification-inactive");
+    notification.classList.add("notification-inactive");
+    notification.classList.remove("notification-success");
     timeout = null;
   }, duration);
 }
